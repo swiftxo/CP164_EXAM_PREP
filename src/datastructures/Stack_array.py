e68e395d3a42bc202740d93abd945b8567ebe203
Sohan <hoss1529@mylaurier.ca>
2025-07-31 10:18:16 -0400

    labs 1 to 8

---
+++ src/datastructures/Stack_array.py
@@ -0,0 +1,102 @@
+"""
+-------------------------------------------------------
+Array version of the Stack ADT.
+-------------------------------------------------------
+Author:  David Brown
+ID:      123456789
+Email:   dbrown@wlu.ca
+__updated__ = "2024-01-15"
+-------------------------------------------------------
+"""
+# Imports
+from copy import deepcopy
+
+
+class Stack:
+
+    def __init__(self):
+        """
+        -------------------------------------------------------
+        Initializes an empty stack. Data is stored in a Python list.
+        Use: s = Stack()
+        -------------------------------------------------------
+        Returns:
+            a new Stack object (Stack)
+        -------------------------------------------------------
+        """
+        self._values = []
+
+    def is_empty(self):
+        """
+        -------------------------------------------------------
+        Determines if the stack is empty.
+        Use: b = s.is_empty()
+        -------------------------------------------------------
+        Returns:
+            True if the stack is empty, False otherwise
+        -------------------------------------------------------
+        """
+
+        # Your code here
+		
+    def push(self, value):
+        """
+        -------------------------------------------------------
+        Pushes a copy of value onto the top of the stack.
+        Use: s.push(value)
+        -------------------------------------------------------
+        Parameters:
+            value - a data element (?)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+
+        # Your code here
+
+    def pop(self):
+        """
+        -------------------------------------------------------
+        Pops and returns the top of stack. The value is removed
+        from the stack. Attempting to pop from an empty stack
+        throws an exception.
+        Use: value = s.pop()
+        -------------------------------------------------------
+        Returns:
+            value - the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot pop from an empty stack"
+
+        # Your code here
+
+    def peek(self):
+        """
+        -------------------------------------------------------
+        Returns a copy of the value at the top of the stack.
+        Attempting to peek at an empty stack throws an exception.
+        Use: value = s.peek()
+        -------------------------------------------------------
+        Returns:
+            value - a copy of the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot peek at an empty stack"
+
+        # Your code here
+
+
+    def __iter__(self):
+        """
+        FOR TESTING ONLY
+        -------------------------------------------------------
+        Generates a Python iterator. Iterates through the stack
+        from top to bottom.
+        Use: for v in s:
+        -------------------------------------------------------
+        Returns:
+            value - the next value in the stack (?)
+        -------------------------------------------------------
+        """
+        for value in self._values[::-1]:
+            yield value
---
+++ src/lab_05_recursion/functions.py
@@ -0,0 +1,82 @@
+
+def recurse(x, y):
+    """
+    -------------------------------------------------------
+    Recursive function - example of tree recursion.
+    Use: ans = recurse(x, y)
+    -------------------------------------------------------
+    Parameters:
+        x - an integer (int)
+        y - an integer (int)
+    Returns:
+        ans - the function result (int)
+    -------------------------------------------------------
+    """
+
+def gcd(m, n):
+    """
+    -------------------------------------------------------
+    Recursively find the Greatest Common Denominator of two numbers.
+    Use: ans = gcd(m, n)
+    -------------------------------------------------------
+    Parameters:
+        n - an integer (int)
+        m - an integer (int)
+    Returns:
+        ans - the function result (int)
+    -------------------------------------------------------
+    """
+
+def vowel_count(s):
+    """
+    -------------------------------------------------------
+    Recursively counts number of vowels in a string.
+    Use: count = vowel_count(s)
+    -------------------------------------------------------
+    Parameters:
+        s - string to examine (str)
+    Returns:
+        count - number of vowels in s (int)
+    -------------------------------------------------------
+    """
+
+def to_power(base, power):
+    """
+    -------------------------------------------------------
+    Calculates base^power.
+    Use: ans = to_power(base, power)
+    -------------------------------------------------------
+    Parameters:
+        base - base to apply power to (float)
+        power - power to apply (int)
+    Returns:
+        ans - base ^ power (float)
+    -------------------------------------------------------
+    """
+
+def is_palindrome(s):
+    """
+    -------------------------------------------------------
+    Recursively determines if s is a palindrome. Ignores non-letters and case.
+    Use: palindrome = is_palindrome(s)
+    -------------------------------------------------------
+    Parameters:
+    s - a string (str)
+    Returns:
+    palindrome - True if s is a palindrome, False otherwise (boolean)
+    -------------------------------------------------------
+    """
+
+
+def bag_to_set(bag):
+    """
+    -------------------------------------------------------
+    Copies elements of a bag to a set.
+    Use: new_set = bag_to_set(bag)
+    -------------------------------------------------------
+    Parameters:
+    bag - a list of values (list)
+    Returns:
+    new_set - containing one each of the elements in bag (list)
+    -------------------------------------------------------
+    """
